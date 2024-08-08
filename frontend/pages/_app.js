8d39480d8bf9a05eeba221deb30efb05f3530412
Todd Garland <todd@toddgarland.com>
2024-08-08 11:05:51 -0400

    Testing out a new approach

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,12 @@
-import { FullresTag } from 'fullres-nextjs';
+import { FullresProvider } from 'fullres-nextjs';
 import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <FullresTag siteKey={process.env.NEXT_PUBLIC_FULLRES_SITEKEY} proxy={process.env.NEXT_PUBLIC_FULLRES_PROXY} />
-      <Component {...pageProps} />
-    </Layout>
+    <FullresProvider siteKey={process.env.NEXT_PUBLIC_FULLRES_SITEKEY} proxy={process.env.NEXT_PUBLIC_FULLRES_PROXY}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </FullresProvider>
   );
 }
